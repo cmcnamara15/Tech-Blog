ab5f2b081601f00f2fd305fc304d56c00ae72075
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-07 14:30:46 -0500

    Add index file to controllers folder

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,10 +1,21 @@
 const Sequalize = require('sequalize');
 require('dotenv').config();
 
-let sequalize;
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequalize = new Sequalize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequalize = new Sequalize
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 }
+
+module.exports = sequelize;
