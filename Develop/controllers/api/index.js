cbd47feb2f929e9f3033e91b18abf0a242084c1d
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-07 14:47:26 -0500

    Add post route to postRoutes.js

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
+// need  comment route
 router.use('/user', userRoutes);
 router.use('/posts', postRoutes);
 
