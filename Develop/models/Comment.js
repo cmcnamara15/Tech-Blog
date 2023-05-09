d823f72a020a9387e3b0d0c509ae1ef5bd45da85
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-08 21:02:40 -0500

    Add column to comment.js model

--- Develop/models/Comment.js
+++ Develop/models/Comment.js
@@ -5,6 +5,14 @@ class Comment extends Model {}
 
 Comment.init(
     {
-        
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING
+        },
     }
 )
