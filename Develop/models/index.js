e43b7c2a76ca277e854cbc79328a48bfecd8ec6a
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-13 14:31:30 -0500

    Continue code for index model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -2,7 +2,7 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-User.hasMant(Post, {
+User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
@@ -10,3 +10,11 @@ User.hasMant(Post, {
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
