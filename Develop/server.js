7482f3742c25bdfb7f8a0e25cc34ee8e16505dad
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-17 13:18:49 -0500

    Add required files to index.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+
 
