e861f7e43a043c72d501f3b1a9b01b393a2ec4aa
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-17 13:32:00 -0500

    Sync sequelize in index.js

--- Develop/server.js
+++ Develop/server.js
@@ -30,3 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
