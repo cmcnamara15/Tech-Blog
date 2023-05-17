40750453a0617b2f2248ab4d34edab717e507b4c
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-17 13:26:36 -0500

    Add session variable to index.js

--- Develop/server.js
+++ Develop/server.js
@@ -11,4 +11,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {}
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
