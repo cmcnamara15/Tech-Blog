0c8f9f0ed2fad69557a153547c14a7d5163c6ed8
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-17 13:30:27 -0500

    Add middleware to index.js

--- Develop/server.js
+++ Develop/server.js
@@ -20,3 +20,13 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+app.use(routes);
