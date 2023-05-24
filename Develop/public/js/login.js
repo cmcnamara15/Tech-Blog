8791626c0bac6313dc2fcde63219c2273ea0230b
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-24 17:54:18 -0500

    Add handleSubmit function to login.js

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -1,2 +1,13 @@
 const loginForm = document.querySelector("#login-form")
-loginForm.addEventListener("submit", )
+
+function handleSubmit(){
+    const userName = document.querySelector("#userName")
+    const password = document.querySelector("#password")
+    fetch()
+    .then(res => res.json)
+    .then(data => {
+        console.log(data)
+    })
+}
+
+loginForm.addEventListener("submit", handleSubmit)
