04f1c65d4a5066ef3da6a0c17179de03f3f1a650
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-25 15:29:53 -0500

    Export commentRoutes.js

--- Develop/controllers/api/commentRoutes.js
+++ Develop/controllers/api/commentRoutes.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models/');
+
+
+
+module.exports = router
--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
 
 router.get('/', (req, res) => {
     res.render("home", {
