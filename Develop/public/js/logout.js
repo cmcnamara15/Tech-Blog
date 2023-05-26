bc3c5cec1e90a38172c853b6ef840e5cd5d69f6f
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-26 16:09:09 -0500

    Add form to logout.handlebars

---
+++ Develop/public/js/logout.js
@@ -0,0 +1,18 @@
+const logoutLink = document.querySelector("#logout")
+
+function handleSubmit(event){
+    event.preventDefault()
+
+    fetch("/api/users/logout", {
+        method: "POST",
+    }) 
+    .then(data => {
+        // if(data.message === "You are now logged in!") {
+            window.location.href="/"
+        // } else {
+        //     window.alert("login error")
+        // }
+    })
+}
+
+logoutLink.addEventListener("click", handleSubmit)
