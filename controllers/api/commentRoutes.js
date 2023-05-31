77a804db74307fb20bba4c28a403626dc6e676e8
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-30 23:02:47 -0500

    Add color and style to blog post feed

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,16 +3,16 @@ const { Post, User, Comment } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-      const newPost = await Comment.create({
+        const newPost = await Comment.create({
         ...req.body,
         user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newPost);
+        });
+        
+        res.status(200).json(newPost);
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  });
+    });
 
 
 
